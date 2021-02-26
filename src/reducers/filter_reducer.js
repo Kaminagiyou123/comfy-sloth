de7d88e69117ba287e51496bc6c4139644c9e27e
Ran You <ran.you8311@gmail.com>
2021-02-26 11:11:45 -0800

    sort function complete

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -16,6 +16,40 @@ const filter_reducer = (state, action) => {
       filtered_products:[...action.payload]
     };
   }
+  if (action.type===SET_LISTVIEW){
+    return {...state,
+     grid_view:false
+    };
+  }
+  if (action.type===SET_GRIDVIEW){
+    return {...state,
+     grid_view:true
+    };
+  }
+  if (action.type===UPDATE_SORT){
+    return {...state,sort:action.payload}
+  }
+  if (action.type===SORT_PRODUCTS){
+    const {sort,filtered_products}=state;
+    let tempProducts=[...filtered_products];
+    if (sort==='price-lowest'){
+      tempProducts=tempProducts.sort((a,b)=>a.price-b.price)
+    }
+    if (sort==='price-highest'){
+      tempProducts=tempProducts.sort((b,a)=>a.price-b.price)    
+    }
+    if (sort==='name-a'){
+      tempProducts=tempProducts.sort((a,b)=>a.name.localeCompare(b.name)) 
+    }
+    if (sort==='name-z'){
+      
+      tempProducts=tempProducts.sort((b,a)=>a.name.localeCompare(b.name)) 
+      
+    }
+return {...state,filtered_products:tempProducts}
+  }
+
+  
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
