ddf07bb5eae63b48e1571f19080be03512a9c656
Ran You <ran.you8311@gmail.com>
2021-02-26 18:44:22 -0800

    add to cart complete

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,6 +6,7 @@ import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
 const AddToCart = ({product}) => {
+  const {addToCart}=useCartContext()
   const {id,stock,colors}= product;
   const [mainColor,setMainColor]=useState(colors[0]);
   const [amount,setAmount]=useState(1);
@@ -46,7 +47,9 @@ return tempAmount
     <div className='btn-container'>
       <AmountButtons amount={amount} decrease={decrease}
       increase={increase}/>
-      <Link to ='/cart' className='btn'>Add to Cart</Link>
+      <Link to ='/cart' className='btn' onClick={()=>{
+        addToCart(id,mainColor,amount,product)
+      }}>Add to Cart</Link>
     </div>
   </Wrapper>
 }
