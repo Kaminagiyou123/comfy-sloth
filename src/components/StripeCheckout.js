1fef55d88af6b7cfbf9dc5cae181a10ace5cd0c8
Ran You <ran.you8311@gmail.com>
2021-03-06 14:22:35 -0800

    initial commit

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -12,44 +12,119 @@ import { useCartContext } from '../context/cart_context'
 import { useUserContext } from '../context/user_context'
 import { formatPrice } from '../utils/helpers'
 import { useHistory } from 'react-router-dom'
+const promise=loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
+
 
 const CheckoutForm = () => {
-  return <h4>hello from Stripe Checkout </h4>
+  const {cart,total_amount,shipping_fee,clearCart}=useCartContext()
+  const {myUser}=useUserContext()
+  const history=useHistory()
+  //stripe stuff
+  const [succeeded, setSucceeded]=useState(false);
+  const [error, setError]=useState(null);
+  const [processing,setProccessing]=useState('');
+  const [disabled,setDisabled]=useState(true);
+  const [clientSecret,setClientSecret]=useState('');
+  const stripe=useState();
+  const elements=useElements()
+
+  const cardStyle = {
+    style: {
+      base: {
+        color: '#32325d',
+        fontFamily: 'Arial, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+          color: '#32325d',
+        },
+      },
+      invalid: {
+        color: '#fa755a',
+        iconColor: '#fa755a',
+      },
+    },
+  }
+
+  const createPaymentIntent= async()=>{
+    try { const {data} = await axios.post(
+      '/.netlify/functions/create-payment-intent',
+      JSON.stringify({cart,shipping_fee,total_amount}) 
+    )
+    setClientSecret(data.clientSecret)
+    console.log(data)
+    } catch(error){
+      console.log(error.response);
+    }
+  }
+
+  useEffect (()=>{
+    createPaymentIntent()
+  },[])
+
+  const handleChange=async(event)=>{}
+
+  const handleSubmit=async(event)=>{}
+
+  return <div>
+    <form id='payment-form' onSubmit={handleSubmit}>
+      <CardElement id='card-element' options={cardStyle} onChange={handleChange}/>
+    <button disabled={processing ||disabled|| succeeded} id="submit">
+      <span id='button-text'>{processing ?
+      <div className='spinner' id='spinner'/>:'Pay'}</span> 
+    </button>
+
+{/* show any error when processing the payment */}
+{error && <div className='card-error' roll='alert'>{error}</div>}
+{/* show a success msg upon completion */}
+<p className={succeeded ? "result-message" : "result-message hidden"}>
+        Payment succeeded, see the result in your
+        <a
+          href={`https://dashboard.stripe.com/test/payments`}
+        >
+          {" "}
+          Stripe dashboard.
+        </a> Refresh the page to pay again.
+      </p>
+    </form>
+  </div>
 }
 
 const StripeCheckout = () => {
   return (
     <Wrapper>
+      <Elements stripe={promise}>
       <CheckoutForm />
+      </Elements>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
   form {
-    width: 30vw;
-    align-self: center;
-    box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1),
-      0px 2px 5px 0px rgba(50, 50, 93, 0.1),
-      0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
-    border-radius: 7px;
-    padding: 40px;
-  }
-  input {
-    border-radius: 6px;
-    margin-bottom: 6px;
-    padding: 12px;
-    border: 1px solid rgba(50, 50, 93, 0.1);
-    max-height: 44px;
-    font-size: 16px;
-    width: 100%;
-    background: white;
-    box-sizing: border-box;
-  }
-  .result-message {
-    line-height: 22px;
-    font-size: 16px;
-  }
+  width: 30vw;
+  align-self: center;
+  box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1),
+    0px 2px 5px 0px rgba(50, 50, 93, 0.1), 0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
+  border-radius: 7px;
+  padding: 40px;
+}
+
+input {
+  border-radius: 6px;
+  margin-bottom: 6px;
+  padding: 12px;
+  border: 1px solid rgba(50, 50, 93, 0.1);
+  max-height: 44px;
+  font-size: 16px;
+  width: 100%;
+  background: white;
+  box-sizing: border-box;
+}
+.result-message {
+  line-height: 22px;
+  font-size: 16px;
+}
   .result-message a {
     color: rgb(89, 111, 214);
     font-weight: 600;
